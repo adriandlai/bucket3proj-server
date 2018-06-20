9246ff3cc81e55d306497423352afbc85b9a9a2c
Administrator <adriandlai@gmail.com>
2018-06-20 14:14:17 -0600

    undid hardcoding for stripe api key

--- constants/stripe.js
+++ constants/stripe.js
@@ -1,9 +1,9 @@
 const configureStripe = require('stripe');
 
-// const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-//     ? 'sk_test_qFZ0OsuGL2jD4NGfO2polcBC'
-//     : 'sk_test_qFZ0OsuGL2jD4NGfO2polcBC';
+const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
+    ? 'sk_test_qFZ0OsuGL2jD4NGfO2polcBC'
+    : 'sk_test_qFZ0OsuGL2jD4NGfO2polcBC';
 
-// const stripe = configureStripe(STRIPE_SECRET_KEY);
-const stripe = configureStripe('sk_test_qFZ0OsuGL2jD4NGfO2polcBC');
+const stripe = configureStripe(STRIPE_SECRET_KEY);
+
 module.exports = stripe;
